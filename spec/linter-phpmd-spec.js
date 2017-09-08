7b6a453d9006e1a96744ead1b5377b3a03f44b18
Landon Abney <Landonabney@gmail.com>
2017-09-08 10:36:17 -0700

    :shirt: Fix linting issues

--- spec/linter-phpmd-spec.js
+++ spec/linter-phpmd-spec.js
@@ -2,7 +2,7 @@
 
 import * as path from 'path';
 
-const lint = require('../lib/main.js').provideLinter().lint;
+const { lint } = require('../lib/main.js').provideLinter();
 
 const goodPath = path.join(__dirname, 'files', 'good.php');
 const badPath = path.join(__dirname, 'files', 'bad.php');
@@ -16,18 +16,14 @@ describe('The phpmd provider for Linter', () => {
         atom.packages.activatePackage('linter-phpmd'),
         atom.packages.activatePackage('language-php'),
       ]).then(() =>
-        atom.workspace.open(goodPath),
-      ),
-    );
+        atom.workspace.open(goodPath)));
   });
 
   it('should be in the packages list', () =>
-    expect(atom.packages.isPackageLoaded('linter-phpmd')).toBe(true),
-  );
+    expect(atom.packages.isPackageLoaded('linter-phpmd')).toBe(true));
 
   it('should be an active package', () =>
-    expect(atom.packages.isPackageActive('linter-phpmd')).toBe(true),
-  );
+    expect(atom.packages.isPackageActive('linter-phpmd')).toBe(true));
 
   describe('checks bad.php and', () => {
     let editor = null;
@@ -35,16 +31,13 @@ describe('The phpmd provider for Linter', () => {
       waitsForPromise(() =>
         atom.workspace.open(badPath).then((openEditor) => {
           editor = openEditor;
-        }),
-      );
+        }));
     });
 
     it('finds at least one message', () => {
       waitsForPromise(() =>
         lint(editor).then(messages =>
-          expect(messages.length).toBeGreaterThan(0),
-        ),
-      );
+          expect(messages.length).toBeGreaterThan(0)));
     });
 
     it('verifies the first message', () => {
@@ -56,8 +49,7 @@ describe('The phpmd provider for Linter', () => {
             '::a(). The configured minimum method name length is 3.');
           expect(messages[0].filePath).toBe(badPath);
           expect(messages[0].range).toEqual([[1, 0], [1, 14]]);
-        }),
-      );
+        }));
     });
   });
 
@@ -65,19 +57,11 @@ describe('The phpmd provider for Linter', () => {
     waitsForPromise(() =>
       atom.workspace.open(emptyPath).then(editor =>
         lint(editor).then(messages =>
-          expect(messages.length).toBe(0),
-        ),
-      ),
-    ),
-  );
+          expect(messages.length).toBe(0)))));
 
   it('finds nothing wrong with a valid file', () =>
     waitsForPromise(() =>
       atom.workspace.open(goodPath).then(editor =>
         lint(editor).then(messages =>
-          expect(messages.length).toBe(0),
-        ),
-      ),
-    ),
-  );
+          expect(messages.length).toBe(0)))));
 });
